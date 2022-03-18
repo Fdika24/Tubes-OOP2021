c2b41cbebe547fbdc347103aac577f4743f64dba
farhan <18220015@std.stei.itb.ac.id>
2022-03-18 22:38:40 +0700

    selection menu prototype added

--- nintendo-tubes-2021/src/com/company/MainView.java
+++ nintendo-tubes-2021/src/com/company/MainView.java
@@ -17,6 +17,11 @@ public class MainView extends UIViewController {
     protected void viewDidLoad() {
         super.viewDidLoad();
         System.out.println("View has been loaded...");
+        System.out.println("Welcome to Piku Monsters!");
+        System.out.println("Menus : ");
+        System.out.println("1. Start Game");
+        System.out.println("2. Start Game");
+        System.out.println("3. Exit Game");
     }
 
     // called when you pop the view
@@ -31,6 +36,10 @@ public class MainView extends UIViewController {
     protected void viewDidFinnish() {
         super.viewDidFinnish();
         System.out.println("View has been removed from stack...");
+    }
+
+    private void didSelectExitGame() {
+        System.out.println("Thank you for playing!");
     }
 }
 
---
+++ nintendo-tubes-2021/src/com/company/rootFolder/View/SelectionView.java
@@ -0,0 +1,10 @@
+package com.company.rootFolder.View;
+
+import com.company.extention.UIViewController;
+
+public class SelectionView extends UIViewController {
+    @Override
+    protected void viewDidLoad() {
+        super.viewDidLoad();
+    }
+}
