5f016515d35d43f67d22f749486c52d1a24c53a5
farhan <18220015@std.stei.itb.ac.id>
2022-03-18 22:53:46 +0700

    added element list

--- nintendo-tubes-2021/src/com/company/rootFolder/model/Element.java
+++ nintendo-tubes-2021/src/com/company/rootFolder/model/Element.java
@@ -2,10 +2,10 @@ package com.company.rootFolder.model;
 
 public class Element {
     public final ElementType type;
-    public final Element target;
+    public final ElementType target;
     public final double effectivity;
 
-    public Element(ElementType type, Element target, double effectivity) {
+    public Element(ElementType type, ElementType target, double effectivity) {
         this.type = type;
         this.target = target;
         this.effectivity = effectivity;
