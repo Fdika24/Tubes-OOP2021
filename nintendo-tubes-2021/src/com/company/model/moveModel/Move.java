a6bd5ca62232f885bb3262dae1ed01612158f989
farhan <18220015@std.stei.itb.ac.id>
2022-03-25 20:29:52 +0700

    added effect and base stats

--- nintendo-tubes-2021/src/com/company/model/moveModel/Move.java
+++ nintendo-tubes-2021/src/com/company/model/moveModel/Move.java
@@ -1,6 +1,7 @@
 package com.company.model.moveModel;
 
 import com.company.model.ElementType;
+import com.company.model.monsters.Stats;
 
 public class Move {
     public final int id;
@@ -11,10 +12,21 @@ public class Move {
     public final int priority;
     public int ammunition;
     public final MoveTarget target;
+    private final Stats effect;
+    public final double baseAttack;
+    public final EffectType effectType;
 
-    public Move(int id, MoveType moveType,
-                String name, ElementType elementType,
-                int accuracy, int priority, int ammunition, MoveTarget target) {
+    public Move(int id,
+                MoveType moveType,
+                String name,
+                ElementType elementType,
+                int accuracy,
+                int priority,
+                int ammunition,
+                MoveTarget target,
+                Stats effect,
+                EffectType effectType
+                ) {
         this.id = id;
         this.name = name;
         this.elementType = elementType;
@@ -23,5 +35,13 @@ public class Move {
         this.ammunition = ammunition;
         this.moveType = moveType;
         this.target = target;
+        this.effect = effect;
+        this.baseAttack = effect.getAttackPoint();
+        this.effectType = effectType;
+    }
+
+
+    public Stats getMoveEffect() {
+        return this.effect;
     }
 }
